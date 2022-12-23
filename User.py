6eee5fe744d548a79f17d95cf8f99a308e0e9a1e
PuGaLo1337 <bigbabyban228@gmail.com>
2022-12-24 00:40:15 +0500

    Пример № 11

---
+++ User.py
@@ -0,0 +1,4 @@
+UserName = input("Твое имя?")
+AgeUser = input("Сколько тебе лет?")
+LiveUser = input("Где ты живешь?")
+print("Тебя зовут: {0}\nТебе {1} лет\nТы живешь в городе  {2}".format(UserName, AgeUser, LiveUser))
