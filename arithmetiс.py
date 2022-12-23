5ca03c6adc203f20773083dc6cda4a1176bee6b9
PuGaLo1337 <bigbabyban228@gmail.com>
2022-12-24 00:15:41 +0500

    Пример № 9

---
+++ arithmetiс.py
@@ -0,0 +1,5 @@
+youranswer = int(input("Решите пример 4 * 100 - 54 = "))
+if youranswer == 346:
+    print("Ваш ответ {0}. Правильный".format(youranswer))
+else:
+    print("Ваш ответ {0}. Неправильный".format(youranswer))
