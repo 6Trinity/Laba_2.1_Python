d47690e733a01b321db6fccb8f3678e56176ae34
PuGaLo <100396548+PuGaLo1337@users.noreply.github.com>
2023-04-22 03:43:31 +0500

    Add files via upload

---
+++ Project/numbers.py
@@ -0,0 +1,6 @@
+number1 = int(input("Введите первое число"))
+number2 = int(input("Введите второе число"))
+number3 = int(input("Введите третье число"))
+number4 = int(input("Введите четвертое число"))
+Oyvet = ((number1+number2)/(number3+number4))
+print("Ответ: {0}".format(Oyvet))
